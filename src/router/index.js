be9055b57ede8e929b0609c493cca83079888385
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-11-13 20:43:45 +0900

    サインイン/サインアップ機能

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import firebase from "firebase";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,7 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: { requiresAuth: true },
   },
   {
     path: "/process",
@@ -28,12 +30,50 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/TodoList.vue"),
   },
+  {
+    path: "/signin",
+    name: "signin",
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/Authorization/signin.vue"
+      ),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/Authorization/signup.vue"
+      ),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+        next();
+      } else {
+        console.log(user);
+        next({
+          path: "/signin",
+          query: { redirect: to.fullPath },
+        });
+      }
+    });
+  } else {
+    next(); // next() を常に呼び出すようにしてください!
+  }
 });
 
 export default router;
---
+++ src/views/Authorization/signup.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>サインアップページ</h1>
+
+    <input type="email" v-model="email" placeholder="example@mail.com" />
+    <input type="password" v-model="password" placeholder="パスワード" />
+    <input
+      type="password"
+      v-model="password2"
+      placeholder="パスワード(確認用)"
+    />
+    <button v-on:click="signup">サインアップ</button>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      password2: "",
+    };
+  },
+  methods: {
+    signup() {
+      if (this.email == null || this.email == "") {
+        alert("メアドが未入力です");
+        return;
+      }
+      if (this.password == null || this.password == "") {
+        alert("パスワードが未入力です");
+        return;
+      }
+      if (this.password != this.password2) {
+        alert("パスワードが一致しません");
+        return;
+      }
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+          alert(
+            "ログインに失敗しました エラーコード" +
+              errorCode +
+              "エラーメッセージ" +
+              errorMessage
+          );
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
