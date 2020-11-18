5bdc49c0b74d8c7a8012df3a04556d852b3bf8ed
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-11-18 23:07:03 +0900

    グーグルアカウントでログイン
    Googleアカウントでログイン機能を実装しました。

--- src/views/Authorization/signin.vue
+++ src/views/Authorization/signin.vue
@@ -5,6 +5,8 @@
     <input type="email" v-model="email" placeholder="example@mail.com" />
     <input type="password" v-model="password" placeholder="パスワード" />
     <button v-on:click="login">ログイン</button>
+
+    <button v-on:click="google">Googleアカウントでログイン</button>
   </div>
 </template>
 
@@ -44,6 +46,44 @@ export default {
               errorCode +
               "エラーメッセージ" +
               errorMessage
+          );
+        });
+    },
+    google() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(
+          /* function(result) */ () => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            /*           var token = result.credential.accessToken; */
+            // The signed-in user info.
+            /*           var user = result.user; */
+            // ...
+            /*           alert("token : " + token + "User : " + user); */
+            this.$router.push("/");
+          }
+        )
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+          alert(
+            "ログインに失敗しました エラーコード" +
+              errorCode +
+              "エラーメッセージ" +
+              errorMessage +
+              "email : " +
+              email +
+              "credential : " +
+              credential
           );
         });
     },
