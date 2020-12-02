fa11bb3eea40827b5ec7c2e79e2ab3c33bf1141d
Arisa-K <aripa54321@gmail.com>
2020-12-03 01:44:18 +0900

    home&process_css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import "./firebase.js"; // これを追加
 import router from "./router";
 import Vuex from "vuex";
 import store from "./store";
+//fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(Vuex);
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>ホームです</h1>
     目標 :<input type="text" v-model="goaltext" v-on:keyup.enter="addgoal" />
     状態 :<input type="checkbox" v-model="status" v-on:keyup.enter="addgoal" />
     期限 :<input type="date" v-model="timelimit" v-on:keyup.enter="addgoal" />
     <button v-on:click="addgoal" v-on:keyup.enter="addgoal">
       送信
     </button>
-    <div v-for="(obj, index) in goalList" :key="index">
-      {{ obj.text }}, {{ obj.status }}, {{ obj.timelimit }}
+    <div class="goal-area">
+      <div class="page-title">GOALS</div>
+      <div v-for="(obj, index) in goalList" :key="index">
+        {{ obj.status }}, 
+        <div class="card-base">
+          <div class="card-status-icon">
+            <font-awesome-icon icon="cloud" class="cloud" />
+          </div>
+          <div class="card-contents">
+            <div class="card-contents-title">
+              {{ obj.text }}
+            </div>
+            <div class="card-contents-timelimit">
+              {{ obj.timelimit }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <button v-on:click="logout">ログアウト</button>
   </div>
@@ -103,3 +117,62 @@ export default {
   },
 };
 </script>
+<style>
+.home{
+  margin: auto;
+}
+.page-title{
+  width: 100%;
+  height: 50px;
+  font-family: "Noto Sans JP";
+  font-weight: bold;
+  font-size: 24px;
+  color: #3D9E8D;
+  line-height: 50px;
+  letter-spacing: 0.05em;
+  text-align: center;
+}
+.goal-area{
+  align-items: center;
+}
+.card-base{
+  width: 335px;
+  height: 50px;
+  background-color: white;
+  display: flex;
+  padding: 10px;
+  border-radius: 10px;
+}
+/* アイコンに関して */
+.card-status-icon{
+  width: 50px; 
+  margin: auto;
+  text-align: center;
+}
+.svg-inline--fa.fa-w-20{
+  width: 30px;
+  height: 20px;
+  margin: 10px;
+  color: #3D9E8D;
+}
+
+/* カードの内容に関して */
+.card-contents{
+  width: 285px;
+}
+.card-contents-title{
+  height: 26px;
+  font-weight: bold;
+  font-size: 15.5px;
+  line-height: 25px;
+}
+
+.card-contents-timelimit{
+  height: 20px;
+  font-family: "Noto Sans JP";
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 20px;
+  color: #757575;
+}
+</style>
