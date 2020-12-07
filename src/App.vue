660c6287d15ba482910107d1d768d69d3c2e06f5
Arisa-K <aripa54321@gmail.com>
2020-12-07 14:58:10 +0900

    sign_in_design

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #383838;
   background: #f2e9e3;
   min-height: 800px;
   font-family: "Noto Sans JP";
